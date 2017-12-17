6b825ab6c48a813e49b55d8014dc6f3de7cb3321
CarefulNow <danny25106@gmail.com>
2017-12-17 11:58:33 +0000

    Added another service layer test

---
+++ src/test/java/com/bazinga/riverguides/api/service/AllRiversServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.bazinga.riverguides.api.service;
+
+import com.bazinga.riverguides.api.dao.impl.AllRiversDAOImpl;
+import com.bazinga.riverguides.api.models.River;
+import com.bazinga.riverguides.api.models.RiverList;
+import com.bazinga.riverguides.api.service.impl.AllRiversServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+public class AllRiversServiceImplTest {
+    @InjectMocks
+    private AllRiversServiceImpl allRiversService;
+
+    @Mock
+    private AllRiversDAOImpl allRiversDAO;
+
+    @Test
+    public void testGetAllRivers() {
+        River river1 = new River();
+        river1.setRiverName("river1");
+        River river2 = new River();
+        river2.setRiverName("river2");
+        List<River> riverList = new ArrayList<>();
+        riverList.add(river1);
+        riverList.add(river2);
+        when(allRiversDAO.getAllRivers()).thenReturn(riverList);
+
+        RiverList result = allRiversService.getAllRivers();
+        assertEquals("The list in Riverlist contains two entries", 2, result.getRivers().size());
+        assertEquals("River 1 is in the list", "river1", result.getRivers().get(0).getRiverName());
+        assertEquals("River 2 is in the list", "river2", result.getRivers().get(1).getRiverName());
+    }
+}
